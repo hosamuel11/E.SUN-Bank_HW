f9c6c89a90289328dd054018093dc2b72725f9b7
samuelho <samuelho00921@intumit.com>
2024-04-16 20:17:12 +0800

    更改db

--- DB/Employee_insertsql.sql
+++ DB/Employee_insertsql.sql
@@ -1,4 +1,4 @@
 INSERT INTO Employee (EMP_ID,NAME, EMAIL) VALUES
 (16241,'John Doe', 'john.doe@example.com'),
 (16256,'Jane Smith', 'jane.smith@example.com'),
-(16222,'Bob Johnson', 'bob123.johnson@example.com');
+(16222,'Bob Johnson', 'bob123312312.johnson@example.com');
